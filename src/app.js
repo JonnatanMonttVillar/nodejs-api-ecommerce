931868d2a7b679c6720f405998d0b4093ec3dd37
Jonnatan Montt Villar <jonnatan_montt@hotmail.com>
2021-02-28 17:52:03 -0300

    Mejoramiento api

--- src/app.js
+++ src/app.js
@@ -1,10 +1,4 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('status: OK')
-})
-
-app.listen(3000, ()=>{
-    console.log("server is running http://localhost:3000");
-})
+module.exports = app;
